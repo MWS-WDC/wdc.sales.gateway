a509034a94a0cc7a54a8c7a3f02340c01eb08a34
Maghfera-Alhussaini <magihus77@gmail.com>
2025-05-30 10:01:16 +0300

    Add user service to gateway

--- Wdc.Sales.Gateway/Program.cs
+++ Wdc.Sales.Gateway/Program.cs
@@ -1,19 +1,43 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Text;
 
 namespace Wdc.Sales.Gateway
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddAuthorization();
+            builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            builder.Services.AddOcelot();
+
+            builder.Services.AddAuthentication("JwtBearer")
+                .AddJwtBearer("JwtBearer", options =>
+                {
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                    };
+                });
+
 
             var app = builder.Build();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            await app.UseOcelot();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -23,28 +47,9 @@ namespace Wdc.Sales.Gateway
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
-
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
-
             app.Run();
         }
     }
 }
+
+
